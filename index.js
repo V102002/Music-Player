be89b1226ded418b64123382ea97ab5305110fe7
V102002 <vssnsvs@gmail.com>
2025-04-23 18:04:35 +0530

    Add index.js complete

--- index.js
+++ index.js
@@ -47,9 +47,54 @@ function playMusic(){
     isPlaying=true;
     //Change the flag variable to true and change the playbutton to pause
     playBtn.classList.replace('fa-play','fa-pause');
+    playBtn.setAttribute('title','Pause');
+    music.play();
 }
 
 function pauseMusic(){
     isPlaying=false;
     playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+    music.pause();
 }
+
+function loadMusic(song){
+    music.src=song.path;
+    title.textContent=song.displayName;
+    artist.textContent=song.artist;
+    image.src=song.cover;
+    background.src=song.cover
+}
+
+function changeMusic(direction){
+    musicIndex=(musicIndex+direction+songs.length)%songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar(){
+    const {duration,currentTime}=music;
+    const progressPercent=(currentTime/duration)*100;
+    progress.style.width=`${progressPercent}%`;
+
+
+    const formatTime=(time)=>String(Math.floor(time)).padStart(2,'0');
+    durationEl.textContent=`${formatTime(duration/60)}:${formatTime(duration%60)}`; 
+    currentTimeEl.textContent=`${formatTime(currentTime/60)}:${formatTime(currentTime%60)}`;
+
+}
+
+function setProgressBar(e){
+    const width=playerProgress.clientWidth;
+    const clickX=e.offsetX;
+    music.currentTime=(clickX/width)*music.duration;
+}
+
+playBtn.addEventListener('click',togglePlay);
+nextBtn.addEventListener('click',()=>changeMusic(-1));
+prevBtn.addEventListener('click',()=>changeMusic(1));
+music.addEventListener('ended',()=>changeMusic(1));
+music.addEventListener('timeupdate',updateProgressBar);
+playerProgress.addEventListener('click',setProgressBar);
+
+loadMusic(songs[musicIndex]);
